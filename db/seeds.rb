49cd9e47a4839fd447b147d97fd1a1dc3c399539
Bobby BuffaloBoy <novenarii@gmail.com>
2020-08-30 16:11:03 -0400

    added postgres DATABASE_URL(from heroku) to database.yml file

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,8 +6,8 @@
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
 
-tod = User.create(first_name: 'Larry', last_name: "BuffaloBoy", password: 'password123', password_confirmation: 'password123', username: 'TronicData')
-greg = User.create(first_name: 'Bobby', last_name: "BuffaloBoy", password: 'password123',password_confirmation: 'password123', username: 'SlickJacket')
+Larry = User.create(first_name: 'Larry', last_name: "BuffaloBoy", password: 'password123', password_confirmation: 'password123', username: 'TronicData')
+Bobby = User.create(first_name: 'Bobby', last_name: "BuffaloBoy", password: 'password123',password_confirmation: 'password123', username: 'SlickJacket')
 # f1 = Friendship.create(user_id: tod.id, friend_id: greg.id)
 # Act1 = ActionInfo.create(name: "working", user_id: tod.id)
 
