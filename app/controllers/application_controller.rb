6d4f12def6323640871e1620357e58b1d529614f
tronicdata <larrybuffaloboy@gmail.com>
2020-09-01 18:45:42 -0400

    Changes to try for heroku

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,8 +1,6 @@
 class ApplicationController < ActionController::API
-    before_action :authorized
-    protect_from_forgery
-    before_filter :cors_preflight_check
-    after_filter :cors_set_access_control_headers
+  before_filter :set_access
+
 
     def cors_set_access_control_headers
       headers['Access-Control-Allow-Origin'] = '*'
@@ -11,15 +9,18 @@ class ApplicationController < ActionController::API
       headers['Access-Control-Max-Age'] = "1728000"
     end
     
-    def cors_preflight_check
-      if request.method == "OPTIONS"
-        headers['Access-Control-Allow-Origin'] = '*'
-        headers['Access-Control-Allow-Methods'] = 'POST, GET, OPTIONS'
-        headers['Access-Control-Allow-Headers'] = %w{Origin Accept Content-Type X-Requested-With auth_token X-CSRF-Token}.join(',')
-        headers['Access-Control-Max-Age'] = '1728000'
-        render :text => '', :content_type => 'text/plain'
-      end
+    def set_access 
+      @response.headers["Access-Control-Allow-Origin"] = "*"
     end
+    # def cors_preflight_check
+    #   if request.method == "OPTIONS"
+    #     headers['Access-Control-Allow-Origin'] = '*'
+    #     headers['Access-Control-Allow-Methods'] = 'POST, GET, OPTIONS'
+    #     headers['Access-Control-Allow-Headers'] = %w{Origin Accept Content-Type X-Requested-With auth_token X-CSRF-Token}.join(',')
+    #     headers['Access-Control-Max-Age'] = '1728000'
+    #     render :text => '', :content_type => 'text/plain'
+    #   end
+    # end
 
     def secret
         Rails.application.credentials[:jwt_secret]
